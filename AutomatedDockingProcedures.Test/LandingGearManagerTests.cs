41ef918aeea0424dbab5698425471b43062efc9f
JakenVeina <jakenveina@yahoo.com>
2019-02-24 23:18:00 -0600

    Implemented LandingGearManager and corresponding tests.

---
+++ AutomatedDockingProcedures.Test/LandingGearManagerTests.cs
@@ -0,0 +1,215 @@
+﻿using System.Linq;
+
+using Moq;
+using NUnit.Framework;
+using Shouldly;
+using static IngameScript.Program;
+
+using SpaceEngineers.Game.ModAPI.Ingame;
+
+namespace AutomatedDockingProcedures.Test
+{
+    [TestFixture]
+    public class LandingGearManagerTests
+    {
+        #region Test Context
+
+        private class TestContext
+        {
+            public TestContext()
+            {
+                Uut = new LandingGearManager();
+            }
+
+            public readonly LandingGearManager Uut;
+        }
+
+        #endregion Test Context
+
+        #region AddLandingGear() Tests
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(10)]
+        public void AddLandingGear_Always_AddsLandingGear(int existingLandingGearCount)
+        {
+            var testContext = new TestContext();
+
+            var mockExistingLandingGears = Enumerable.Repeat(0, existingLandingGearCount)
+                .Select(_ => new Mock<IMyLandingGear>())
+                .ToArray();
+            
+            foreach(var mockExistingLandingGear in mockExistingLandingGears)
+                testContext.Uut.AddLandingGear(mockExistingLandingGear.Object);
+
+            var mockLandingGear = new Mock<IMyLandingGear>();
+
+            testContext.Uut.AddLandingGear(mockLandingGear.Object);
+
+            testContext.Uut.LandingGears.ShouldContain(mockLandingGear.Object);
+        }
+
+        #endregion AddLandingGear() Tests
+
+        #region ClearLandingGears() Tests
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(10)]
+        public void ClearLandingGears_Always_ClearsLandingGears(int landingGearCount)
+        {
+            var testContext = new TestContext();
+
+            var mockLandingGears = Enumerable.Repeat(0, landingGearCount)
+                .Select(_ => new Mock<IMyLandingGear>())
+                .ToArray();
+
+            foreach (var mockLandingGear in mockLandingGears)
+                testContext.Uut.AddLandingGear(mockLandingGear.Object);
+
+            testContext.Uut.ClearLandingGears();
+
+            testContext.Uut.LandingGears.ShouldBeEmpty();
+        }
+
+        #endregion ClearLandingGears() Tests
+
+        #region MakeLockOperation() Tests
+
+        [Test]
+        public void MakeLockOperation_LandingGearsIsEmpty_CompletesImmediately()
+        {
+            var testContext = new TestContext();
+
+            testContext.Uut.MakeLockOperation()
+                .ShouldRunToCompletionIn(1);
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        public void MakeLockOperation_LandingGearsIsNotEmpty_LocksEachLandingGear(int landingGearCount)
+        {
+            var testContext = new TestContext();
+
+            var mockLandingGears = Enumerable.Repeat(0, landingGearCount)
+                .Select(_ => new Mock<IMyLandingGear>())
+                .ToArray();
+
+            foreach (var mockLandingGear in mockLandingGears)
+                testContext.Uut.AddLandingGear(mockLandingGear.Object);
+
+            testContext.Uut.MakeLockOperation()
+                .ShouldRunToCompletionIn(landingGearCount);
+
+            mockLandingGears.ForEach(mockLandingGear =>
+                mockLandingGear.ShouldHaveReceived(x => x.Lock()));
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        public void MakeLockOperation_OperationIsDisposed_RecyclesOperation(int landingGearCount)
+        {
+            var testContext = new TestContext();
+
+            var mockLandingGears = Enumerable.Repeat(0, landingGearCount)
+                .Select(_ => new Mock<IMyLandingGear>())
+                .ToArray();
+
+            foreach (var mockLandingGear in mockLandingGears)
+                testContext.Uut.AddLandingGear(mockLandingGear.Object);
+
+            var result = testContext.Uut.MakeLockOperation();
+            result.ShouldRunToCompletionIn(landingGearCount);
+
+            var mockLandingGearInvocations = mockLandingGears
+                .Select(x => x.Invocations.ToArray())
+                .ToArray();
+
+            mockLandingGears.ForEach(x => x
+                .Invocations.Clear());
+
+            testContext.Uut.MakeLockOperation()
+                .ShouldBeSameAs(result);
+
+            result.ShouldRunToCompletionIn(landingGearCount);
+
+            foreach(var i in Enumerable.Range(0, mockLandingGears.Length))
+            {
+                mockLandingGears[i].Invocations.Count.ShouldBe(mockLandingGearInvocations[i].Length);
+                foreach (var j in Enumerable.Range(0, mockLandingGears[i].Invocations.Count))
+                    mockLandingGears[i].Invocations[j].ShouldBe(mockLandingGearInvocations[i][j]);
+            }
+        }
+
+        #endregion MakeLockOperation() Tests
+
+        #region MakeUnlockOperation() Tests
+
+        [Test]
+        public void MakeUnlockOperation_LandingGearsIsEmpty_CompletesImmediately()
+        {
+            var testContext = new TestContext();
+
+            testContext.Uut.MakeUnlockOperation()
+                .ShouldRunToCompletionIn(1);
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        public void MakeUnlockOperation_LandingGearsIsNotEmpty_UnlocksEachLandingGear(int landingGearCount)
+        {
+            var testContext = new TestContext();
+
+            var mockLandingGears = Enumerable.Repeat(0, landingGearCount)
+                .Select(_ => new Mock<IMyLandingGear>())
+                .ToArray();
+
+            foreach (var mockLandingGear in mockLandingGears)
+                testContext.Uut.AddLandingGear(mockLandingGear.Object);
+
+            testContext.Uut.MakeUnlockOperation()
+                .ShouldRunToCompletionIn(landingGearCount);
+
+            mockLandingGears.ForEach(mockLandingGear =>
+                mockLandingGear.ShouldHaveReceived(x => x.Unlock()));
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        public void MakeUnlockOperation_OperationIsDisposed_RecyclesOperation(int landingGearCount)
+        {
+            var testContext = new TestContext();
+
+            var mockLandingGears = Enumerable.Repeat(0, landingGearCount)
+                .Select(_ => new Mock<IMyLandingGear>())
+                .ToArray();
+
+            foreach (var mockLandingGear in mockLandingGears)
+                testContext.Uut.AddLandingGear(mockLandingGear.Object);
+
+            var result = testContext.Uut.MakeUnlockOperation();
+            result.ShouldRunToCompletionIn(landingGearCount);
+
+            var mockLandingGearInvocations = mockLandingGears
+                .Select(x => x.Invocations.ToArray())
+                .ToArray();
+
+            mockLandingGears.ForEach(x => x
+                .Invocations.Clear());
+
+            testContext.Uut.MakeUnlockOperation()
+                .ShouldBeSameAs(result);
+
+            result.ShouldRunToCompletionIn(landingGearCount);
+
+            foreach (var i in Enumerable.Range(0, mockLandingGears.Length))
+            {
+                mockLandingGears[i].Invocations.Count.ShouldBe(mockLandingGearInvocations[i].Length);
+                foreach (var j in Enumerable.Range(0, mockLandingGears[i].Invocations.Count))
+                    mockLandingGears[i].Invocations[j].ShouldBe(mockLandingGearInvocations[i][j]);
+            }
+        }
+
+        #endregion MakeUnlockOperation() Tests
+    }
+}
---
+++ AutomatedDockingProcedures/LandingGearManager.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+
+using SpaceEngineers.Game.ModAPI.Ingame;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public interface ILandingGearManager
+        {
+            IReadOnlyList<IMyLandingGear> LandingGears { get; }
+
+            void AddLandingGear(IMyLandingGear landingGear);
+
+            void ClearLandingGears();
+
+            IBackgroundOperation MakeLockOperation();
+
+            IBackgroundOperation MakeUnlockOperation();
+        }
+
+        public class LandingGearManager : ILandingGearManager
+        {
+            public LandingGearManager()
+            {
+                _lockOperationPool = new ObjectPool<LockOperation>(onFinished
+                    => new LockOperation(this, onFinished));
+
+                _unlockOperationPool = new ObjectPool<UnlockOperation>(onFinished
+                    => new UnlockOperation(this, onFinished));
+            }
+
+            public IReadOnlyList<IMyLandingGear> LandingGears
+                => _landingGears;
+
+            public void AddLandingGear(IMyLandingGear landingGear)
+                => _landingGears.Add(landingGear);
+
+            public void ClearLandingGears()
+                => _landingGears.Clear();
+
+            public IBackgroundOperation MakeLockOperation()
+                => _lockOperationPool.Get();
+
+            public IBackgroundOperation MakeUnlockOperation()
+                => _unlockOperationPool.Get();
+
+            private readonly List<IMyLandingGear> _landingGears
+                = new List<IMyLandingGear>();
+
+            private readonly ObjectPool<LockOperation> _lockOperationPool;
+
+            private readonly ObjectPool<UnlockOperation> _unlockOperationPool;
+
+            private abstract class LandingGearOperationBase : IBackgroundOperation, IDisposable
+            {
+                public LandingGearOperationBase(LandingGearManager owner, Action onDisposed)
+                {
+                    _owner = owner;
+                    _onDisposed = onDisposed;
+
+                    Reset();
+                }
+
+                public BackgroundOperationResult Execute(Action<IBackgroundOperation> subOperationScheduler)
+                {
+                    if (_owner._landingGears.Count == 0)
+                        return BackgroundOperationResult.Completed;
+
+                    OnExecuting(_owner._landingGears[_landingGearIndex]);
+
+                    if (++_landingGearIndex < _owner._landingGears.Count)
+                        return BackgroundOperationResult.NotCompleted;
+
+                    return BackgroundOperationResult.Completed;
+                }
+
+                public void Dispose()
+                {
+                    Reset();
+                    _onDisposed.Invoke();
+                }
+
+                protected abstract void OnExecuting(IMyLandingGear connector);
+
+                private void Reset()
+                    => _landingGearIndex = 0;
+
+                private readonly LandingGearManager _owner;
+
+                private readonly Action _onDisposed;
+
+                private int _landingGearIndex;
+            }
+
+            private sealed class LockOperation : LandingGearOperationBase
+            {
+                public LockOperation(LandingGearManager owner, Action onDisposed)
+                    : base(owner, onDisposed) { }
+
+                protected override void OnExecuting(IMyLandingGear connector)
+                    => connector.Lock();
+            }
+
+            private sealed class UnlockOperation : LandingGearOperationBase
+            {
+                public UnlockOperation(LandingGearManager owner, Action onDisposed)
+                    : base(owner, onDisposed) { }
+
+                protected override void OnExecuting(IMyLandingGear connector)
+                    => connector.Unlock();
+            }
+        }
+    }
+}
