b99e87bb2974aab0d9c8ff853a1bca999fb5ed5f
JakenVeina <jakenveina@yahoo.com>
2019-02-24 23:26:06 -0600

    Implemented FunctionalBlockManager and corresponding tests.

---
+++ AutomatedDockingProcedures.Test/FunctionalBlockManagerTests.cs
@@ -0,0 +1,215 @@
+﻿using System.Linq;
+
+using Moq;
+using NUnit.Framework;
+using Shouldly;
+using static IngameScript.Program;
+
+using Sandbox.ModAPI.Ingame;
+
+namespace AutomatedDockingProcedures.Test
+{
+    [TestFixture]
+    public class FunctionalBlockManagerTests
+    {
+        #region Test Context
+
+        private class TestContext
+        {
+            public TestContext()
+            {
+                Uut = new FunctionalBlockManager();
+            }
+
+            public readonly FunctionalBlockManager Uut;
+        }
+
+        #endregion Test Context
+
+        #region AddFunctionalBlock() Tests
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(10)]
+        public void AddFunctionalBlock_Always_AddsFunctionalBlock(int existingFunctionalBlockCount)
+        {
+            var testContext = new TestContext();
+
+            var mockExistingFunctionalBlocks = Enumerable.Repeat(0, existingFunctionalBlockCount)
+                .Select(_ => new Mock<IMyFunctionalBlock>())
+                .ToArray();
+            
+            foreach(var mockExistingFunctionalBlock in mockExistingFunctionalBlocks)
+                testContext.Uut.AddFunctionalBlock(mockExistingFunctionalBlock.Object);
+
+            var mockFunctionalBlock = new Mock<IMyFunctionalBlock>();
+
+            testContext.Uut.AddFunctionalBlock(mockFunctionalBlock.Object);
+
+            testContext.Uut.FunctionalBlocks.ShouldContain(mockFunctionalBlock.Object);
+        }
+
+        #endregion AddFunctionalBlock() Tests
+
+        #region ClearFunctionalBlocks() Tests
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(10)]
+        public void ClearFunctionalBlocks_Always_ClearsFunctionalBlocks(int functionalBlockCount)
+        {
+            var testContext = new TestContext();
+
+            var mockFunctionalBlocks = Enumerable.Repeat(0, functionalBlockCount)
+                .Select(_ => new Mock<IMyFunctionalBlock>())
+                .ToArray();
+
+            foreach (var mockFunctionalBlock in mockFunctionalBlocks)
+                testContext.Uut.AddFunctionalBlock(mockFunctionalBlock.Object);
+
+            testContext.Uut.ClearFunctionalBlocks();
+
+            testContext.Uut.FunctionalBlocks.ShouldBeEmpty();
+        }
+
+        #endregion ClearFunctionalBlocks() Tests
+
+        #region MakeDisableOperation() Tests
+
+        [Test]
+        public void MakeDisableOperation_FunctionalBlocksIsEmpty_CompletesImmediately()
+        {
+            var testContext = new TestContext();
+
+            testContext.Uut.MakeDisableOperation()
+                .ShouldRunToCompletionIn(1);
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        public void MakeDisableOperation_FunctionalBlocksIsNotEmpty_LocksEachFunctionalBlock(int functionalBlockCount)
+        {
+            var testContext = new TestContext();
+
+            var mockFunctionalBlocks = Enumerable.Repeat(0, functionalBlockCount)
+                .Select(_ => new Mock<IMyFunctionalBlock>())
+                .ToArray();
+
+            foreach (var mockFunctionalBlock in mockFunctionalBlocks)
+                testContext.Uut.AddFunctionalBlock(mockFunctionalBlock.Object);
+
+            testContext.Uut.MakeDisableOperation()
+                .ShouldRunToCompletionIn(functionalBlockCount);
+
+            mockFunctionalBlocks.ForEach(mockFunctionalBlock =>
+                mockFunctionalBlock.ShouldHaveReceivedSet(x => x.Enabled = false));
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        public void MakeDisableOperation_OperationIsDisposed_RecyclesOperation(int functionalBlockCount)
+        {
+            var testContext = new TestContext();
+
+            var mockFunctionalBlocks = Enumerable.Repeat(0, functionalBlockCount)
+                .Select(_ => new Mock<IMyFunctionalBlock>())
+                .ToArray();
+
+            foreach (var mockFunctionalBlock in mockFunctionalBlocks)
+                testContext.Uut.AddFunctionalBlock(mockFunctionalBlock.Object);
+
+            var result = testContext.Uut.MakeDisableOperation();
+            result.ShouldRunToCompletionIn(functionalBlockCount);
+
+            var mockFunctionalBlockInvocations = mockFunctionalBlocks
+                .Select(x => x.Invocations.ToArray())
+                .ToArray();
+
+            mockFunctionalBlocks.ForEach(x => x
+                .Invocations.Clear());
+
+            testContext.Uut.MakeDisableOperation()
+                .ShouldBeSameAs(result);
+
+            result.ShouldRunToCompletionIn(functionalBlockCount);
+
+            foreach(var i in Enumerable.Range(0, mockFunctionalBlocks.Length))
+            {
+                mockFunctionalBlocks[i].Invocations.Count.ShouldBe(mockFunctionalBlockInvocations[i].Length);
+                foreach (var j in Enumerable.Range(0, mockFunctionalBlocks[i].Invocations.Count))
+                    mockFunctionalBlocks[i].Invocations[j].ShouldBe(mockFunctionalBlockInvocations[i][j]);
+            }
+        }
+
+        #endregion MakeDisableOperation() Tests
+
+        #region MakeEnableOperation() Tests
+
+        [Test]
+        public void MakeEnableOperation_FunctionalBlocksIsEmpty_CompletesImmediately()
+        {
+            var testContext = new TestContext();
+
+            testContext.Uut.MakeEnableOperation()
+                .ShouldRunToCompletionIn(1);
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        public void MakeEnableOperation_FunctionalBlocksIsNotEmpty_UnlocksEachFunctionalBlock(int functionalBlockCount)
+        {
+            var testContext = new TestContext();
+
+            var mockFunctionalBlocks = Enumerable.Repeat(0, functionalBlockCount)
+                .Select(_ => new Mock<IMyFunctionalBlock>())
+                .ToArray();
+
+            foreach (var mockFunctionalBlock in mockFunctionalBlocks)
+                testContext.Uut.AddFunctionalBlock(mockFunctionalBlock.Object);
+
+            testContext.Uut.MakeEnableOperation()
+                .ShouldRunToCompletionIn(functionalBlockCount);
+
+            mockFunctionalBlocks.ForEach(mockFunctionalBlock =>
+                mockFunctionalBlock.ShouldHaveReceivedSet(x => x.Enabled = true));
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        public void MakeEnableOperation_OperationIsDisposed_RecyclesOperation(int functionalBlockCount)
+        {
+            var testContext = new TestContext();
+
+            var mockFunctionalBlocks = Enumerable.Repeat(0, functionalBlockCount)
+                .Select(_ => new Mock<IMyFunctionalBlock>())
+                .ToArray();
+
+            foreach (var mockFunctionalBlock in mockFunctionalBlocks)
+                testContext.Uut.AddFunctionalBlock(mockFunctionalBlock.Object);
+
+            var result = testContext.Uut.MakeEnableOperation();
+            result.ShouldRunToCompletionIn(functionalBlockCount);
+
+            var mockFunctionalBlockInvocations = mockFunctionalBlocks
+                .Select(x => x.Invocations.ToArray())
+                .ToArray();
+
+            mockFunctionalBlocks.ForEach(x => x
+                .Invocations.Clear());
+
+            testContext.Uut.MakeEnableOperation()
+                .ShouldBeSameAs(result);
+
+            result.ShouldRunToCompletionIn(functionalBlockCount);
+
+            foreach (var i in Enumerable.Range(0, mockFunctionalBlocks.Length))
+            {
+                mockFunctionalBlocks[i].Invocations.Count.ShouldBe(mockFunctionalBlockInvocations[i].Length);
+                foreach (var j in Enumerable.Range(0, mockFunctionalBlocks[i].Invocations.Count))
+                    mockFunctionalBlocks[i].Invocations[j].ShouldBe(mockFunctionalBlockInvocations[i][j]);
+            }
+        }
+
+        #endregion MakeEnableOperation() Tests
+    }
+}
---
+++ AutomatedDockingProcedures/FunctionalBlockManager.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Sandbox.ModAPI.Ingame;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public interface IFunctionalBlockManager
+        {
+            IReadOnlyList<IMyFunctionalBlock> FunctionalBlocks { get; }
+
+            void AddFunctionalBlock(IMyFunctionalBlock functionalBlock);
+
+            void ClearFunctionalBlocks();
+
+            IBackgroundOperation MakeDisableOperation();
+
+            IBackgroundOperation MakeEnableOperation();
+        }
+
+        public class FunctionalBlockManager : IFunctionalBlockManager
+        {
+            public FunctionalBlockManager()
+            {
+                _disableOperationPool = new ObjectPool<DisableOperation>(onFinished
+                    => new DisableOperation(this, onFinished));
+
+                _enableOperationPool = new ObjectPool<EnableOperation>(onFinished
+                    => new EnableOperation(this, onFinished));
+            }
+
+            public IReadOnlyList<IMyFunctionalBlock> FunctionalBlocks
+                => _functionalBlocks;
+
+            public void AddFunctionalBlock(IMyFunctionalBlock functionalBlock)
+                => _functionalBlocks.Add(functionalBlock);
+
+            public void ClearFunctionalBlocks()
+                => _functionalBlocks.Clear();
+
+            public IBackgroundOperation MakeDisableOperation()
+                => _disableOperationPool.Get();
+
+            public IBackgroundOperation MakeEnableOperation()
+                => _enableOperationPool.Get();
+
+            private readonly List<IMyFunctionalBlock> _functionalBlocks
+                = new List<IMyFunctionalBlock>();
+
+            private readonly ObjectPool<DisableOperation> _disableOperationPool;
+
+            private readonly ObjectPool<EnableOperation> _enableOperationPool;
+
+            private abstract class FunctionalBlockOperationBase : IBackgroundOperation, IDisposable
+            {
+                public FunctionalBlockOperationBase(FunctionalBlockManager owner, Action onDisposed)
+                {
+                    _owner = owner;
+                    _onDisposed = onDisposed;
+
+                    Reset();
+                }
+
+                public BackgroundOperationResult Execute(Action<IBackgroundOperation> subOperationScheduler)
+                {
+                    if (_owner._functionalBlocks.Count == 0)
+                        return BackgroundOperationResult.Completed;
+
+                    OnExecuting(_owner._functionalBlocks[_functionalBlockIndex]);
+
+                    if (++_functionalBlockIndex < _owner._functionalBlocks.Count)
+                        return BackgroundOperationResult.NotCompleted;
+
+                    return BackgroundOperationResult.Completed;
+                }
+
+                public void Dispose()
+                {
+                    Reset();
+                    _onDisposed.Invoke();
+                }
+
+                protected abstract void OnExecuting(IMyFunctionalBlock functionalBlock);
+
+                private void Reset()
+                    => _functionalBlockIndex = 0;
+
+                private readonly FunctionalBlockManager _owner;
+
+                private readonly Action _onDisposed;
+
+                private int _functionalBlockIndex;
+            }
+
+            private sealed class DisableOperation : FunctionalBlockOperationBase
+            {
+                public DisableOperation(FunctionalBlockManager owner, Action onDisposed)
+                    : base(owner, onDisposed) { }
+
+                protected override void OnExecuting(IMyFunctionalBlock functionalBlock)
+                    => functionalBlock.Enabled = false;
+            }
+
+            private sealed class EnableOperation : FunctionalBlockOperationBase
+            {
+                public EnableOperation(FunctionalBlockManager owner, Action onDisposed)
+                    : base(owner, onDisposed) { }
+
+                protected override void OnExecuting(IMyFunctionalBlock functionalBlock)
+                    => functionalBlock.Enabled = true;
+            }
+        }
+    }
+}
