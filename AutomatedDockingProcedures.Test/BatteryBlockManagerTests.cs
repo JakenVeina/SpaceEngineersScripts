380f12552337ce07eabc0cee856858690e766ccb
JakenVeina <jakenveina@yahoo.com>
2019-02-24 23:37:14 -0600

    Implemented BatteryBlockManager and corresponding tests.

---
+++ AutomatedDockingProcedures.Test/BatteryBlockManagerTests.cs
@@ -0,0 +1,223 @@
+﻿using System.Linq;
+
+using Moq;
+using NUnit.Framework;
+using Shouldly;
+using static IngameScript.Program;
+
+using Sandbox.ModAPI.Ingame;
+
+namespace AutomatedDockingProcedures.Test
+{
+    [TestFixture]
+    public class BatteryBlockManagerTests
+    {
+        #region Test Context
+
+        private class TestContext
+        {
+            public TestContext()
+            {
+                Uut = new BatteryBlockManager();
+            }
+
+            public readonly BatteryBlockManager Uut;
+        }
+
+        #endregion Test Context
+
+        #region AddBatteryBlock() Tests
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(10)]
+        public void AddBatteryBlock_Always_AddsBatteryBlock(int existingBatteryBlockCount)
+        {
+            var testContext = new TestContext();
+
+            var mockExistingBatteryBlocks = Enumerable.Repeat(0, existingBatteryBlockCount)
+                .Select(_ => new Mock<IMyBatteryBlock>())
+                .ToArray();
+            
+            foreach(var mockExistingBatteryBlock in mockExistingBatteryBlocks)
+                testContext.Uut.AddBatteryBlock(mockExistingBatteryBlock.Object);
+
+            var mockBatteryBlock = new Mock<IMyBatteryBlock>();
+
+            testContext.Uut.AddBatteryBlock(mockBatteryBlock.Object);
+
+            testContext.Uut.BatteryBlocks.ShouldContain(mockBatteryBlock.Object);
+        }
+
+        #endregion AddBatteryBlock() Tests
+
+        #region ClearBatteryBlocks() Tests
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(10)]
+        public void ClearBatteryBlocks_Always_ClearsBatteryBlocks(int batteryBlockCount)
+        {
+            var testContext = new TestContext();
+
+            var mockBatteryBlocks = Enumerable.Repeat(0, batteryBlockCount)
+                .Select(_ => new Mock<IMyBatteryBlock>())
+                .ToArray();
+
+            foreach (var mockBatteryBlock in mockBatteryBlocks)
+                testContext.Uut.AddBatteryBlock(mockBatteryBlock.Object);
+
+            testContext.Uut.ClearBatteryBlocks();
+
+            testContext.Uut.BatteryBlocks.ShouldBeEmpty();
+        }
+
+        #endregion ClearBatteryBlocks() Tests
+
+        #region MakeRechargeOperation() Tests
+
+        [Test]
+        public void MakeRechargeOperation_BatteryBlocksIsEmpty_CompletesImmediately()
+        {
+            var testContext = new TestContext();
+
+            testContext.Uut.MakeRechargeOperation()
+                .ShouldRunToCompletionIn(1);
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        public void MakeRechargeOperation_BatteryBlocksIsNotEmpty_RechargesEachBatteryBlock(int batteryBlockCount)
+        {
+            var testContext = new TestContext();
+
+            var mockBatteryBlocks = Enumerable.Repeat(0, batteryBlockCount)
+                .Select(_ => new Mock<IMyBatteryBlock>())
+                .ToArray();
+
+            foreach (var mockBatteryBlock in mockBatteryBlocks)
+                testContext.Uut.AddBatteryBlock(mockBatteryBlock.Object);
+
+            testContext.Uut.MakeRechargeOperation()
+                .ShouldRunToCompletionIn(batteryBlockCount);
+
+            mockBatteryBlocks.ForEach(mockBatteryBlock =>
+            {
+                mockBatteryBlock.ShouldHaveReceivedSet(x => x.SemiautoEnabled = false);
+                mockBatteryBlock.ShouldHaveReceivedSet(x => x.OnlyDischarge = false);
+                mockBatteryBlock.ShouldHaveReceivedSet(x => x.OnlyRecharge = true);
+            });
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        public void MakeRechargeOperation_OperationIsDisposed_RecyclesOperation(int batteryBlockCount)
+        {
+            var testContext = new TestContext();
+
+            var mockBatteryBlocks = Enumerable.Repeat(0, batteryBlockCount)
+                .Select(_ => new Mock<IMyBatteryBlock>())
+                .ToArray();
+
+            foreach (var mockBatteryBlock in mockBatteryBlocks)
+                testContext.Uut.AddBatteryBlock(mockBatteryBlock.Object);
+
+            var result = testContext.Uut.MakeRechargeOperation();
+            result.ShouldRunToCompletionIn(batteryBlockCount);
+
+            var mockBatteryBlockInvocations = mockBatteryBlocks
+                .Select(x => x.Invocations.ToArray())
+                .ToArray();
+
+            mockBatteryBlocks.ForEach(x => x
+                .Invocations.Clear());
+
+            testContext.Uut.MakeRechargeOperation()
+                .ShouldBeSameAs(result);
+
+            result.ShouldRunToCompletionIn(batteryBlockCount);
+
+            foreach(var i in Enumerable.Range(0, mockBatteryBlocks.Length))
+            {
+                mockBatteryBlocks[i].Invocations.Count.ShouldBe(mockBatteryBlockInvocations[i].Length);
+                foreach (var j in Enumerable.Range(0, mockBatteryBlocks[i].Invocations.Count))
+                    mockBatteryBlocks[i].Invocations[j].ShouldBe(mockBatteryBlockInvocations[i][j]);
+            }
+        }
+
+        #endregion MakeRechargeOperation() Tests
+
+        #region MakeDischargeOperation() Tests
+
+        [Test]
+        public void MakeDischargeOperation_BatteryBlocksIsEmpty_CompletesImmediately()
+        {
+            var testContext = new TestContext();
+
+            testContext.Uut.MakeDischargeOperation()
+                .ShouldRunToCompletionIn(1);
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        public void MakeDischargeOperation_BatteryBlocksIsNotEmpty_DischargesEachBatteryBlock(int batteryBlockCount)
+        {
+            var testContext = new TestContext();
+
+            var mockBatteryBlocks = Enumerable.Repeat(0, batteryBlockCount)
+                .Select(_ => new Mock<IMyBatteryBlock>())
+                .ToArray();
+
+            foreach (var mockBatteryBlock in mockBatteryBlocks)
+                testContext.Uut.AddBatteryBlock(mockBatteryBlock.Object);
+
+            testContext.Uut.MakeDischargeOperation()
+                .ShouldRunToCompletionIn(batteryBlockCount);
+
+            mockBatteryBlocks.ForEach(mockBatteryBlock =>
+            {
+                mockBatteryBlock.ShouldHaveReceivedSet(x => x.SemiautoEnabled = false);
+                mockBatteryBlock.ShouldHaveReceivedSet(x => x.OnlyDischarge = true);
+                mockBatteryBlock.ShouldHaveReceivedSet(x => x.OnlyRecharge = false);
+            });
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        public void MakeDischargeOperation_OperationIsDisposed_RecyclesOperation(int batteryBlockCount)
+        {
+            var testContext = new TestContext();
+
+            var mockBatteryBlocks = Enumerable.Repeat(0, batteryBlockCount)
+                .Select(_ => new Mock<IMyBatteryBlock>())
+                .ToArray();
+
+            foreach (var mockBatteryBlock in mockBatteryBlocks)
+                testContext.Uut.AddBatteryBlock(mockBatteryBlock.Object);
+
+            var result = testContext.Uut.MakeDischargeOperation();
+            result.ShouldRunToCompletionIn(batteryBlockCount);
+
+            var mockBatteryBlockInvocations = mockBatteryBlocks
+                .Select(x => x.Invocations.ToArray())
+                .ToArray();
+
+            mockBatteryBlocks.ForEach(x => x
+                .Invocations.Clear());
+
+            testContext.Uut.MakeDischargeOperation()
+                .ShouldBeSameAs(result);
+
+            result.ShouldRunToCompletionIn(batteryBlockCount);
+
+            foreach (var i in Enumerable.Range(0, mockBatteryBlocks.Length))
+            {
+                mockBatteryBlocks[i].Invocations.Count.ShouldBe(mockBatteryBlockInvocations[i].Length);
+                foreach (var j in Enumerable.Range(0, mockBatteryBlocks[i].Invocations.Count))
+                    mockBatteryBlocks[i].Invocations[j].ShouldBe(mockBatteryBlockInvocations[i][j]);
+            }
+        }
+
+        #endregion MakeDischargeOperation() Tests
+    }
+}
---
+++ AutomatedDockingProcedures/BatteryBlockManager.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Sandbox.ModAPI.Ingame;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public interface IBatteryManager
+        {
+            IReadOnlyList<IMyBatteryBlock> BatteryBlocks { get; }
+
+            void AddBatteryBlock(IMyBatteryBlock batteryBlock);
+
+            void ClearBatteryBlocks();
+
+            IBackgroundOperation MakeRechargeOperation();
+
+            IBackgroundOperation MakeDischargeOperation();
+        }
+
+        public class BatteryBlockManager : IBatteryManager
+        {
+            public BatteryBlockManager()
+            {
+                _rechargeOperationPool = new ObjectPool<RechargeOperation>(onFinished
+                    => new RechargeOperation(this, onFinished));
+
+                _dischargeOperationPool = new ObjectPool<DischargeOperation>(onFinished
+                    => new DischargeOperation(this, onFinished));
+            }
+
+            public IReadOnlyList<IMyBatteryBlock> BatteryBlocks
+                => _batteryBlocks;
+
+            public void AddBatteryBlock(IMyBatteryBlock batteryBlock)
+                => _batteryBlocks.Add(batteryBlock);
+
+            public void ClearBatteryBlocks()
+                => _batteryBlocks.Clear();
+
+            public IBackgroundOperation MakeRechargeOperation()
+                => _rechargeOperationPool.Get();
+
+            public IBackgroundOperation MakeDischargeOperation()
+                => _dischargeOperationPool.Get();
+
+            private readonly List<IMyBatteryBlock> _batteryBlocks
+                = new List<IMyBatteryBlock>();
+
+            private readonly ObjectPool<RechargeOperation> _rechargeOperationPool;
+
+            private readonly ObjectPool<DischargeOperation> _dischargeOperationPool;
+
+            private abstract class BatteryOperationBase : IBackgroundOperation, IDisposable
+            {
+                public BatteryOperationBase(BatteryBlockManager owner, Action onDisposed)
+                {
+                    _owner = owner;
+                    _onDisposed = onDisposed;
+
+                    Reset();
+                }
+
+                public BackgroundOperationResult Execute(Action<IBackgroundOperation> subOperationScheduler)
+                {
+                    if (_owner._batteryBlocks.Count == 0)
+                        return BackgroundOperationResult.Completed;
+
+                    OnExecuting(_owner._batteryBlocks[_batteryBlockIndex]);
+
+                    if (++_batteryBlockIndex < _owner._batteryBlocks.Count)
+                        return BackgroundOperationResult.NotCompleted;
+
+                    return BackgroundOperationResult.Completed;
+                }
+
+                public void Dispose()
+                {
+                    Reset();
+                    _onDisposed.Invoke();
+                }
+
+                protected abstract void OnExecuting(IMyBatteryBlock batteryBlock);
+
+                private void Reset()
+                    => _batteryBlockIndex = 0;
+
+                private readonly BatteryBlockManager _owner;
+
+                private readonly Action _onDisposed;
+
+                private int _batteryBlockIndex;
+            }
+
+            private sealed class RechargeOperation : BatteryOperationBase
+            {
+                public RechargeOperation(BatteryBlockManager owner, Action onDisposed)
+                    : base(owner, onDisposed) { }
+
+                protected override void OnExecuting(IMyBatteryBlock batteryBlock)
+                {
+                    batteryBlock.SemiautoEnabled = false;
+                    batteryBlock.OnlyDischarge = false;
+                    batteryBlock.OnlyRecharge = true;
+                }
+            }
+
+            private sealed class DischargeOperation : BatteryOperationBase
+            {
+                public DischargeOperation(BatteryBlockManager owner, Action onDisposed)
+                    : base(owner, onDisposed) { }
+
+                protected override void OnExecuting(IMyBatteryBlock batteryBlock)
+                {
+                    batteryBlock.SemiautoEnabled = false;
+                    batteryBlock.OnlyDischarge = true;
+                    batteryBlock.OnlyRecharge = false;
+                }
+            }
+        }
+    }
+}
