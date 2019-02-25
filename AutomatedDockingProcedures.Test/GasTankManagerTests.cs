14e661c0b75be990c7a2e38d1cfb50d2cb914ae3
JakenVeina <jakenveina@yahoo.com>
2019-02-24 23:47:03 -0600

    Implemented GasTankManager and corresponding tests.

---
+++ AutomatedDockingProcedures.Test/GasTankManagerTests.cs
@@ -0,0 +1,215 @@
+﻿using System.Linq;
+
+using Moq;
+using NUnit.Framework;
+using Shouldly;
+using static IngameScript.Program;
+
+using Sandbox.ModAPI.Ingame;
+
+namespace AutomatedDockingProcedures.Test
+{
+    [TestFixture]
+    public class GasTankManagerTests
+    {
+        #region Test Context
+
+        private class TestContext
+        {
+            public TestContext()
+            {
+                Uut = new GasTankManager();
+            }
+
+            public readonly GasTankManager Uut;
+        }
+
+        #endregion Test Context
+
+        #region AddGasTank() Tests
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(10)]
+        public void AddGasTank_Always_AddsGasTank(int existingGasTankCount)
+        {
+            var testContext = new TestContext();
+
+            var mockExistingGasTanks = Enumerable.Repeat(0, existingGasTankCount)
+                .Select(_ => new Mock<IMyGasTank>())
+                .ToArray();
+            
+            foreach(var mockExistingGasTank in mockExistingGasTanks)
+                testContext.Uut.AddGasTank(mockExistingGasTank.Object);
+
+            var mockGasTank = new Mock<IMyGasTank>();
+
+            testContext.Uut.AddGasTank(mockGasTank.Object);
+
+            testContext.Uut.GasTanks.ShouldContain(mockGasTank.Object);
+        }
+
+        #endregion AddGasTank() Tests
+
+        #region ClearGasTanks() Tests
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(10)]
+        public void ClearGasTanks_Always_ClearsGasTanks(int gasTankCount)
+        {
+            var testContext = new TestContext();
+
+            var mockGasTanks = Enumerable.Repeat(0, gasTankCount)
+                .Select(_ => new Mock<IMyGasTank>())
+                .ToArray();
+
+            foreach (var mockGasTank in mockGasTanks)
+                testContext.Uut.AddGasTank(mockGasTank.Object);
+
+            testContext.Uut.ClearGasTanks();
+
+            testContext.Uut.GasTanks.ShouldBeEmpty();
+        }
+
+        #endregion ClearGasTanks() Tests
+
+        #region MakeStockpileOperation() Tests
+
+        [Test]
+        public void MakeStockpileOperation_GasTanksIsEmpty_CompletesImmediately()
+        {
+            var testContext = new TestContext();
+
+            testContext.Uut.MakeStockpileOperation()
+                .ShouldRunToCompletionIn(1);
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        public void MakeStockpileOperation_GasTanksIsNotEmpty_StockpilesEachGasTank(int gasTankCount)
+        {
+            var testContext = new TestContext();
+
+            var mockGasTanks = Enumerable.Repeat(0, gasTankCount)
+                .Select(_ => new Mock<IMyGasTank>())
+                .ToArray();
+
+            foreach (var mockGasTank in mockGasTanks)
+                testContext.Uut.AddGasTank(mockGasTank.Object);
+
+            testContext.Uut.MakeStockpileOperation()
+                .ShouldRunToCompletionIn(gasTankCount);
+
+            mockGasTanks.ForEach(mockGasTank =>
+                mockGasTank.ShouldHaveReceivedSet(x => x.Stockpile = true));
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        public void MakeStockpileOperation_OperationIsDisposed_RecyclesOperation(int gasTankCount)
+        {
+            var testContext = new TestContext();
+
+            var mockGasTanks = Enumerable.Repeat(0, gasTankCount)
+                .Select(_ => new Mock<IMyGasTank>())
+                .ToArray();
+
+            foreach (var mockGasTank in mockGasTanks)
+                testContext.Uut.AddGasTank(mockGasTank.Object);
+
+            var result = testContext.Uut.MakeStockpileOperation();
+            result.ShouldRunToCompletionIn(gasTankCount);
+
+            var mockGasTankInvocations = mockGasTanks
+                .Select(x => x.Invocations.ToArray())
+                .ToArray();
+
+            mockGasTanks.ForEach(x => x
+                .Invocations.Clear());
+
+            testContext.Uut.MakeStockpileOperation()
+                .ShouldBeSameAs(result);
+
+            result.ShouldRunToCompletionIn(gasTankCount);
+
+            foreach(var i in Enumerable.Range(0, mockGasTanks.Length))
+            {
+                mockGasTanks[i].Invocations.Count.ShouldBe(mockGasTankInvocations[i].Length);
+                foreach (var j in Enumerable.Range(0, mockGasTanks[i].Invocations.Count))
+                    mockGasTanks[i].Invocations[j].ShouldBe(mockGasTankInvocations[i][j]);
+            }
+        }
+
+        #endregion MakeStockpileOperation() Tests
+
+        #region MakeDispenseOperation() Tests
+
+        [Test]
+        public void MakeDispenseOperation_GasTanksIsEmpty_CompletesImmediately()
+        {
+            var testContext = new TestContext();
+
+            testContext.Uut.MakeDispenseOperation()
+                .ShouldRunToCompletionIn(1);
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        public void MakeDispenseOperation_GasTanksIsNotEmpty_DispensesEachGasTank(int gasTankCount)
+        {
+            var testContext = new TestContext();
+
+            var mockGasTanks = Enumerable.Repeat(0, gasTankCount)
+                .Select(_ => new Mock<IMyGasTank>())
+                .ToArray();
+
+            foreach (var mockGasTank in mockGasTanks)
+                testContext.Uut.AddGasTank(mockGasTank.Object);
+
+            testContext.Uut.MakeDispenseOperation()
+                .ShouldRunToCompletionIn(gasTankCount);
+
+            mockGasTanks.ForEach(mockGasTank =>
+                mockGasTank.ShouldHaveReceivedSet(x => x.Stockpile = false));
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        public void MakeDispenseOperation_OperationIsDisposed_RecyclesOperation(int gasTankCount)
+        {
+            var testContext = new TestContext();
+
+            var mockGasTanks = Enumerable.Repeat(0, gasTankCount)
+                .Select(_ => new Mock<IMyGasTank>())
+                .ToArray();
+
+            foreach (var mockGasTank in mockGasTanks)
+                testContext.Uut.AddGasTank(mockGasTank.Object);
+
+            var result = testContext.Uut.MakeDispenseOperation();
+            result.ShouldRunToCompletionIn(gasTankCount);
+
+            var mockGasTankInvocations = mockGasTanks
+                .Select(x => x.Invocations.ToArray())
+                .ToArray();
+
+            mockGasTanks.ForEach(x => x
+                .Invocations.Clear());
+
+            testContext.Uut.MakeDispenseOperation()
+                .ShouldBeSameAs(result);
+
+            result.ShouldRunToCompletionIn(gasTankCount);
+
+            foreach (var i in Enumerable.Range(0, mockGasTanks.Length))
+            {
+                mockGasTanks[i].Invocations.Count.ShouldBe(mockGasTankInvocations[i].Length);
+                foreach (var j in Enumerable.Range(0, mockGasTanks[i].Invocations.Count))
+                    mockGasTanks[i].Invocations[j].ShouldBe(mockGasTankInvocations[i][j]);
+            }
+        }
+
+        #endregion MakeDispenseOperation() Tests
+    }
+}
---
+++ AutomatedDockingProcedures/GasTankManager.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Sandbox.ModAPI.Ingame;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public interface IGasTankManager
+        {
+            IReadOnlyList<IMyGasTank> GasTanks { get; }
+
+            void AddGasTank(IMyGasTank gasTank);
+
+            void ClearGasTanks();
+
+            IBackgroundOperation MakeStockpileOperation();
+
+            IBackgroundOperation MakeDispenseOperation();
+        }
+
+        public class GasTankManager : IGasTankManager
+        {
+            public GasTankManager()
+            {
+                _stockpileOperationPool = new ObjectPool<StockpileOperation>(onFinished
+                    => new StockpileOperation(this, onFinished));
+
+                _dispenseOperationPool = new ObjectPool<DispenseOperation>(onFinished
+                    => new DispenseOperation(this, onFinished));
+            }
+
+            public IReadOnlyList<IMyGasTank> GasTanks
+                => _gasTanks;
+
+            public void AddGasTank(IMyGasTank gasTank)
+                => _gasTanks.Add(gasTank);
+
+            public void ClearGasTanks()
+                => _gasTanks.Clear();
+
+            public IBackgroundOperation MakeStockpileOperation()
+                => _stockpileOperationPool.Get();
+
+            public IBackgroundOperation MakeDispenseOperation()
+                => _dispenseOperationPool.Get();
+
+            private readonly List<IMyGasTank> _gasTanks
+                = new List<IMyGasTank>();
+
+            private readonly ObjectPool<StockpileOperation> _stockpileOperationPool;
+
+            private readonly ObjectPool<DispenseOperation> _dispenseOperationPool;
+
+            private abstract class GasTankOperationBase : IBackgroundOperation, IDisposable
+            {
+                public GasTankOperationBase(GasTankManager owner, Action onDisposed)
+                {
+                    _owner = owner;
+                    _onDisposed = onDisposed;
+
+                    Reset();
+                }
+
+                public BackgroundOperationResult Execute(Action<IBackgroundOperation> subOperationScheduler)
+                {
+                    if (_owner._gasTanks.Count == 0)
+                        return BackgroundOperationResult.Completed;
+
+                    OnExecuting(_owner._gasTanks[_gasTankIndex]);
+
+                    if (++_gasTankIndex < _owner._gasTanks.Count)
+                        return BackgroundOperationResult.NotCompleted;
+
+                    return BackgroundOperationResult.Completed;
+                }
+
+                public void Dispose()
+                {
+                    Reset();
+                    _onDisposed.Invoke();
+                }
+
+                protected abstract void OnExecuting(IMyGasTank gasTank);
+
+                private void Reset()
+                    => _gasTankIndex = 0;
+
+                private readonly GasTankManager _owner;
+
+                private readonly Action _onDisposed;
+
+                private int _gasTankIndex;
+            }
+
+            private sealed class StockpileOperation : GasTankOperationBase
+            {
+                public StockpileOperation(GasTankManager owner, Action onDisposed)
+                    : base(owner, onDisposed) { }
+
+                protected override void OnExecuting(IMyGasTank gasTank)
+                    => gasTank.Stockpile = true;
+            }
+
+            private sealed class DispenseOperation : GasTankOperationBase
+            {
+                public DispenseOperation(GasTankManager owner, Action onDisposed)
+                    : base(owner, onDisposed) { }
+
+                protected override void OnExecuting(IMyGasTank gasTank)
+                    => gasTank.Stockpile = false;
+            }
+        }
+    }
+}
